439736ca119bf409d248f804d08aa04f19f15a91
TrauNguyen <nguyenvanngoctrau@gmail.com>
2023-12-24 20:09:13 -0600

    init Gardening Journal app

---
+++ GardeningJournal/app/src/main/java/com/tvt/gardeningjournal/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tvt.gardeningjournal.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tvt.gardeningjournal.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
